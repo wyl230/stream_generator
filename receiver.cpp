663c8d3861ddcd8b4347ace8cc0e9b0a7ac45463
wyl230 <1900012779@pku.edu.cn>
2023-07-04 17:16:10 +0800

    see

--- receiver.cpp
+++ receiver.cpp
@@ -308,14 +308,17 @@ public:
       if(ptr->tunnel_id == 6) { // 网页
         if(ptr->source_module_id == 100) {
           // 客户端发送给服务器
-          cout << "网页:客户端发送给服务器" << endl;
+          if(should_print_log)
+            cout << "网页:客户端发送给服务器" << endl;
           sockaddr_in duplex_target_addr = get_sockaddr_in(real_address(string("real-data-back")), 23101);
           error = sendto(my_socket, datagram, readLen - sizeof(my_package), 0, (sockaddr *)&duplex_target_addr, sizeof(duplex_target_addr));
           if (error == -1) { perror("sendto"); cout <<"sendto() error occurred at package "<< endl; }
         } else if(ptr->source_module_id == 200) {
           // 服务器发送给客户端
-          cout << "网页：服务器发送给客户端" << endl;
-          cout << duplex_client_address << " " << 23201 << endl;
+          if(should_print_log) {
+            cout << "网页：服务器发送给客户端" << endl;
+            cout << duplex_client_address << " " << 23201 << endl;
+          }
           sockaddr_in duplex_target_addr = get_sockaddr_in(real_address(string("real-data-back")), 23201);
           error = sendto(my_socket, datagram, readLen - sizeof(my_package), 0, (sockaddr *)&duplex_target_addr, sizeof(duplex_target_addr));
           if (error == -1) { perror("sendto"); cout <<"sendto() error occurred at package "<< endl; }
